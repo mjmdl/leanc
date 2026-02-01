fc96c3280dc8aeffe07a228ba4156e5ec696a276
Matheus Maidel <matheus.maidel@gmail.com>
2026-02-01 03:19:07 -0300

    Tokenize symbols.

--- src/lexer.c
+++ src/lexer.c
@@ -111,9 +111,186 @@ static Lexer_Status lexer_tokenize_identifier(Lexer *lexer) {
     return Lexer_Status__Token_Found;
 }
 
+static Token_Kind lexer_peek_symbol(const Lexer *lexer) {
+    switch (*lexer->head) {
+    case '+':
+        if (!lexer_near_end(lexer, 1)) {
+            switch (lexer->head[1]) {
+            case '+':
+                return Token_Kind__Symbol_Plus_Plus;
+            case '=':
+                return Token_Kind__Symbol_Plus_Equal;
+            default:
+                break;
+            }
+        }
+        return Token_Kind__Symbol_Plus;
+
+    case '-':
+        if (!lexer_near_end(lexer, 1)) {
+            switch (lexer->head[1]) {
+            case '-':
+                return Token_Kind__Symbol_Minus_Minus;
+            case '=':
+                return Token_Kind__Symbol_Minus_Equal;
+            case '>':
+                return Token_Kind__Symbol_Arrow;
+            default:
+                break;
+            }
+        }
+        return Token_Kind__Symbol_Minus;
+
+    case '*':
+        if (!lexer_near_end(lexer, 1) && lexer->head[1] == '=') {
+            return Token_Kind__Symbol_Star_Equal;
+        }
+        return Token_Kind__Symbol_Star;
+
+    case '/':
+        if (!lexer_near_end(lexer, 1) && lexer->head[1] == '=') {
+            return Token_Kind__Symbol_Slash_Equal;
+        }
+        return Token_Kind__Symbol_Slash;
+
+    case '%':
+        if (!lexer_near_end(lexer, 1) && lexer->head[1] == '=') {
+            return Token_Kind__Symbol_Modulo_Equal;
+        }
+        return Token_Kind__Symbol_Modulo;
+
+    case '.':
+        if (
+            !lexer_near_end(lexer, 2) &&
+            lexer->head[1] == '.' && lexer->head[2] == '.'
+        ) {
+            return Token_Kind__Symbol_Ellipsis;
+        }
+        return Token_Kind__Symbol_Dot;
+
+    case '=':
+        if (!lexer_near_end(lexer, 1) && lexer->head[1] == '=') {
+            return Token_Kind__Symbol_Equal_Equal;
+        }
+        return Token_Kind__Symbol_Equal;
+
+    case '>':
+        if (!lexer_near_end(lexer, 1)) {
+            if (lexer->head[1] == '=') {
+                return Token_Kind__Symbol_Greater_Equal;
+            }
+            if (lexer->head[1] == '>') {
+                if (!lexer_near_end(lexer, 2) && lexer->head[2] == '=') {
+                    return Token_Kind__Symbol_Shift_Right_Equal;
+                }
+                return Token_Kind__Symbol_Shift_Right;
+            }
+        }
+        return Token_Kind__Symbol_Greater;
+
+    case '<':
+        if (!lexer_near_end(lexer, 1)) {
+            if (lexer->head[1] == '=') {
+                return Token_Kind__Symbol_Less_Equal;
+            }
+            if (lexer->head[1] == '<') {
+                if (!lexer_near_end(lexer, 2) && lexer->head[2] == '=') {
+                    return Token_Kind__Symbol_Shift_Left_Equal;
+                }
+                return Token_Kind__Symbol_Shift_Left;
+            }
+        }
+        return Token_Kind__Symbol_Less;
+
+    case '!':
+        if (!lexer_near_end(lexer, 1) && lexer->head[1] == '=') {
+            return Token_Kind__Symbol_Not_Equal;
+        }
+        return Token_Kind__Symbol_Not;
+
+    case '&':
+        if (!lexer_near_end(lexer, 1)) {
+            if (lexer->head[1] == '&') {
+                return Token_Kind__Symbol_And_And;
+            }
+            if (lexer->head[1] == '=') {
+                return Token_Kind__Symbol_And_Equal;
+            }
+        }
+        return Token_Kind__Symbol_And;
+
+    case '|':
+        if (!lexer_near_end(lexer, 1)) {
+            if (lexer->head[1] == '|') {
+                return Token_Kind__Symbol_Or_Or;
+            }
+            if (lexer->head[1] == '=') {
+                return Token_Kind__Symbol_Or_Equal;
+            }
+        }
+        return Token_Kind__Symbol_Or;
+
+    case '^':
+        if (!lexer_near_end(lexer, 1) && lexer->head[1] == '=') {
+            return Token_Kind__Symbol_Caret_Equal;
+        }
+        return Token_Kind__Symbol_Caret;
+
+    case '~':
+        return Token_Kind__Symbol_Tilde;
+    case ':':
+        return Token_Kind__Symbol_Colon;
+    case ',':
+        return Token_Kind__Symbol_Comma;
+    case '{':
+        return Token_Kind__Symbol_Curly_Left;
+    case '}':
+        return Token_Kind__Symbol_Curly_Right;
+    case '(':
+        return Token_Kind__Symbol_Paren_Left;
+    case ')':
+        return Token_Kind__Symbol_Paren_Right;
+    case '[':
+        return Token_Kind__Symbol_Square_Left;
+    case ']':
+        return Token_Kind__Symbol_Square_Right;
+    case ';':
+        return Token_Kind__Symbol_Semicolon;
+
+    default:
+        break;
+    }
+
+    return Token_Kind__None;
+}
+
+static Lexer_Status lexer_tokenize_symbol(Lexer *lexer) {
+    Token_Kind kind = lexer_peek_symbol(lexer);
+    if (kind == Token_Kind__None) {
+        return Lexer_Status__Ok;
+    }
+
+    Token *symbol = lexer_ready_token(lexer, kind);
+    if (symbol == NULL) {
+        return Lexer_Status__Memory_Failure;
+    }
+
+    symbol->length = token_kind_length(kind);
+    lexer->head += symbol->length;
+
+    lexer_accept_token(lexer, symbol);
+    
+    return Lexer_Status__Token_Found;
+}
+
 static Lexer_Status lexer_tokenize_next(Lexer *lexer) {
-    Lexer_Status status = lexer_tokenize_identifier(lexer);
-    if (status != Lexer_Status__Ok) {
+    Lexer_Status status = Lexer_Status__Ok;
+    
+    if ((status = lexer_tokenize_identifier(lexer)) != Lexer_Status__Ok) {
+        return status;
+    }
+
+    if ((status = lexer_tokenize_symbol(lexer)) != Lexer_Status__Ok) {
         return status;
     }
 
